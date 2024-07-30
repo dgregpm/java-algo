f332bc562f6f6f85516622fcbf22cdaca2086729
dgregpm <dgregpm@gmail.com>
2024-07-30 10:01:19 -0400

    Day 4

---
+++ Dna/Days4/LRU.java
@@ -0,0 +1,16 @@
+public class LRU<K,V> {
+    
+    private int length;
+
+    LRU() {
+    
+    }
+
+    public void update(K key, V value) {
+
+    }
+    
+    public V get(K key) {
+
+    }
+}
---
+++ Dna/Days4/utils/QueueX.java
@@ -0,0 +1,23 @@
+public class QueueX<T> {
+
+    public T deque() {
+        return null;       
+    }
+
+    public void enqueue(T obj){
+    
+    }
+
+    public T peek() {
+        return null;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public String toString(){
+        return null;
+    }
+}
+
